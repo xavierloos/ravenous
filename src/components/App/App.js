e9f5139b7c8870b4bd0f10b14627182532990064
Javier Lopez <javierlopez@MacBook-Pro.home>
2020-08-05 21:21:29 +0100

    Conect to API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Business from '../Business/Business';
 import BusinessList from '../BusinessList/BusinessList';
 import SearcBar from '../SearchBar/SearchBar';
 
-
+const business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+}
+const businesses =[
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
 function App() {
   return (
     <div className="App">
       <h1>ravenous</h1>
       <SearcBar />
-      <BusinessList />
+      <BusinessList businesses={businesses}/>
     </div>
   );
 }
